7a465a827a7957b6861f6e83fe208652d1844beb
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-09-05 07:33:46 +0900

    feat: change types

--- packages/types/src/contestProvider/storage.ts
+++ packages/types/src/contestProvider/storage.ts
@@ -4,22 +4,31 @@ export type JSONPrimitive = string | number | boolean | null;
 export type StorageType<T extends Record<string, JSONPrimitive> = Record<string, JSONPrimitive>> =
   T;
 
-export type FileReadError = { type: "read_error"; message: string };
+export type FileReadError = { type: "read_error"; message: string; code: string | undefined };
 
-export type FileWriteError = { type: "write_error"; message: string };
+export type FileWriteError = { type: "write_error"; message: string; code: string | undefined };
 
 export type JSONParseError = { type: "parse_error"; message: string };
 
 export type JSONStringifyError = { type: "stringify_error"; message: string };
+export type UnexpectedError = { type: "unexpected_error"; message: unknown };
 
 export type StorageContext<T extends StorageType> = {
-  getItem<K extends keyof T>(keyName: K): Result<T[K] | null, FileReadError | JSONParseError>;
+  getItem<K extends keyof T>(
+    keyName: K,
+  ): Result<T[K] | null, FileReadError | JSONParseError | UnexpectedError>;
   setItem<K extends keyof T>(
     keyName: K,
     keyValue: T[K],
-  ): Result<void, FileWriteError | FileReadError | JSONParseError | JSONStringifyError>;
+  ): Result<
+    void,
+    FileWriteError | FileReadError | JSONParseError | JSONStringifyError | UnexpectedError
+  >;
   removeItem<K extends keyof T>(
     keyName: K,
-  ): Result<void, FileWriteError | FileReadError | JSONParseError | JSONStringifyError>;
-  clear(): Result<void, FileWriteError>;
+  ): Result<
+    void,
+    FileWriteError | FileReadError | JSONParseError | JSONStringifyError | UnexpectedError
+  >;
+  clear(): Result<void, FileWriteError | UnexpectedError>;
 };

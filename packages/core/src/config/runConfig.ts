41dc676317274e036c4f97b750bdacbe307e0fae
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-09-04 17:20:21 +0900

    feat: update readConfig

--- packages/core/src/config/runConfig.ts
+++ packages/core/src/config/runConfig.ts
@@ -2,5 +2,10 @@ import { type ResultAsync, fromPromise } from "neverthrow";
 import { createConfigJiti } from "./createJiti.ts";
 
 let jiti = createConfigJiti();
-export const runConfig = (path: string): ResultAsync<unknown, unknown> =>
-  fromPromise(jiti.import(path), (error) => error);
+export const runConfig = (
+  path: string,
+): ResultAsync<unknown, { type: "jiti_error"; error: unknown }> =>
+  fromPromise(jiti.import(path), (error) => ({
+    type: "jiti_error",
+    error,
+  }));
